b8b77c0da9aa46f9a9f3350c36ced4052643f639
Surya Venkatesan <suryaedu71@gmail.com>
2022-01-21 18:02:21 +0530

    Rel v1

--- app/src/main/java/com/sparrow/cactaceae/MainActivity.kt
+++ app/src/main/java/com/sparrow/cactaceae/MainActivity.kt
@@ -2,10 +2,39 @@ package com.sparrow.cactaceae
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tv_counter_space = findViewById<TextView>(R.id.tv_counter_space)
+        var btn_incr = findViewById<Button>(R.id.btn_incr)
+        var btn_decr = findViewById<Button>(R.id.btn_decr)
+
+        btn_incr.setOnClickListener {
+            if(count>=0){
+                count +=1
+                tv_counter_space.text = count.toString()
+
+                Log.i("SKHST_512","$count")
+            }
+        }
+
+        btn_decr.setOnClickListener {
+            if(count>0) {
+                count -= 1
+                tv_counter_space.text = count.toString()
+                Log.i("SKHST_512","$count")
+            }
+        }
+
     }
+
 }
